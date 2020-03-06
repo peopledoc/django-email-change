a0577e2580e2fe3d34f8826bf431891d64ff6060
eric <eric@hellman.net>
2020-03-05 21:39:25 -0500

    set version to 0.7

--- src/email_change/__init__.py
+++ src/email_change/__init__.py
@@ -24,7 +24,7 @@
 #  limitations under the License.
 #
 
-VERSION = (0, 2, 3, 'final', 0)
+VERSION = (0, 7, 0, 'alpha', 0)
 
 def get_version():
     version = '%d.%d.%d' % (VERSION[0], VERSION[1], VERSION[2])
